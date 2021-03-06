260d8b544fce26f6f13da3e625299c985fd7e608
BekzodDeveloper <bkzd18kholdarov@gmail.com>
2021-03-06 14:50:59 +0500

    section 'reviews' is completed

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-$(function(){
+$(function () {
     $('.main__slider').slick({
         arrows: false,
         dots: true,
         autoplay: true,
         fade: true
+    });
+});
+$(function () {
+    $('.reviews__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
     });
 });
